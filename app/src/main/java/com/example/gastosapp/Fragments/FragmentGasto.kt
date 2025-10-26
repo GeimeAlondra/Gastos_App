2f458392948d33531843f96a60ff419e0800428b
GeimeAlondra <105683683+GeimeAlondra@users.noreply.github.com>
2025-10-26 17:52:25 -0600

    Actualización de fragmentos a kotlin (faltan los de presupuesto, esos se van en el siguiente commit)

--- app/src/main/java/com/example/gastosapp/Fragments/FragmentGasto.kt
+++ app/src/main/java/com/example/gastosapp/Fragments/FragmentGasto.kt
@@ -1,19 +1,21 @@
-package com.example.gastosapp.Fragments;
+package com.example.gastosapp.Fragments
 
-import android.os.Bundle;
-import androidx.fragment.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import com.example.gastosapp.R;
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.gastosapp.R
 
-public class FragmentGasto extends Fragment {
+class FragmentGasto : Fragment() {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gasto, container, false);
+        return inflater.inflate(R.layout.fragment_gasto, container, false)
     }
 
 }
--- app/src/main/java/com/example/gastosapp/Fragments/FragmentResumen.kt
+++ app/src/main/java/com/example/gastosapp/Fragments/FragmentResumen.kt
@@ -1,66 +1,21 @@
-package com.example.gastosapp.Fragments;
+package com.example.gastosapp.Fragments
 
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.gastosapp.R
 
-import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+class FragmentResumen : Fragment() {
 
-import com.example.gastosapp.R;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentResumen#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class FragmentResumen extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FragmentResumen() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentResumen.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentResumen newInstance(String param1, String param2) {
-        FragmentResumen fragment = new FragmentResumen();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_resumen, container, false);
+        return inflater.inflate(R.layout.fragment_resumen, container, false)
     }
+
 }
