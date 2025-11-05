7173c320748fcdfadab81606c3baa22836159a6c
IvanMembreno <ivanmembreno168@gmail.com>
2025-11-05 15:07:33 -0600

    Arreglado error de importacion de fragment()

--- app/src/main/java/com/example/gastosapp/Fragments/FragmentPerfil.kt
+++ app/src/main/java/com/example/gastosapp/Fragments/FragmentPerfil.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.gastosapp.LoginActivity
 import com.example.gastosapp.MainActivity
 import com.example.gastosapp.Models.Registro
